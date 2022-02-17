16559af4b777d11a05986cc00f697908b61b654b
Ekaterina Toporkova <90981180+EkaterinaToporkova@users.noreply.github.com>
2022-02-17 11:42:58 +0300

    Delete week_days directory

--- week_days/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from week_days import views as views_week
-
-urlpatterns = [
-    path('<int:day>/', views_week.days_info_by_number),
-    path('<str:day>/', views_week.days_info, name='week-name')
-]
